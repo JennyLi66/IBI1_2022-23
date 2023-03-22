b128db7ec51073812eaf278955aacfe7bc39c6b4
JennyLi66 <127337307+JennyLi66@users.noreply.github.com>
2023-03-22 08:03:28 +0800

    Create File variables.py

---
+++ Practical5/File variables.py
@@ -0,0 +1,22 @@
+Python 3.11.2 (v3.11.2:878ead1ac1, Feb  7 2023, 10:02:41) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Compare the distance between from Edinburgh(a) to Los Angeles(b) and from Edinburgh (a) to Haining(c)
+... #   both the a,b,c,d are variables
+... #   if a = -3.19
+... #   if b = -118.24
+... #   if c = 116.39
+... #   if a-b = d
+... #   if c-a = e
+... #       d =115.05
+... #       e =119.58
+... #   d < e
+... #   Rob travel further to Haining.
+...         
+... 
+... 
+... # if X = True
+... # if Y = False
+... # if W = X and Y
+... # if Z = X or Y
+... # W = False Z = True
+... 

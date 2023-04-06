acf880a5105f4a9167c17d5a5773b12c4336c3e5
JennyLi66 <127337307+JennyLi66@users.noreply.github.com>
2023-04-06 11:14:29 +0800

    Update covid.py

--- Practical 7/covid.py
+++ Practical 7/covid.py
@@ -1,19 +1,5 @@
 Python 3.11.2 (v3.11.2:878ead1ac1, Feb  7 2023, 10:02:41) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
 Type "help", "copyright", "credits" or "license()" for more information.
->>> import os
->>> import pandas as os
->>> import matplotlib.pyplot as plt
->>> import numpy as np
->>> os.chdir("/Users/jenny/IBI_2022-23
-... /Users/jenny/IBI_2022-23
-... 
-SyntaxError: unterminated string literal (detected at line 1)
->>> os.chdir("/Users/jenny/IBI_2022-23/Practical7")
-...          
-Traceback (most recent call last):
-  File "<pyshell#5>", line 1, in <module>
-    os.chdir("/Users/jenny/IBI_2022-23/Practical7")
-AttributeError: module 'pandas' has no attribute 'chdir'
 >>> import pandas as pd
 >>> import os
 >>> import matplotlib.pyplot as plt
@@ -21,29 +7,6 @@ AttributeError: module 'pandas' has no attribute 'chdir'
 >>> os.chdir
 <built-in function chdir>
 >>> os.chdir("/Users/jenny/IBI_2022-23/Practical7")
->>> os.getcwd("/Users/jenny/IBI_2022-23/Practical7")
-Traceback (most recent call last):
-  File "<pyshell#12>", line 1, in <module>
-    os.getcwd("/Users/jenny/IBI_2022-23/Practical7")
-TypeError: posix.getcwd() takes no arguments (1 given)
->>> listdir("/Users/jenny/IBI_2022-23/Practical7")
-Traceback (most recent call last):
-  File "<pyshell#13>", line 1, in <module>
-    listdir("/Users/jenny/IBI_2022-23/Practical7")
-NameError: name 'listdir' is not defined
->>> os.getcwd
-<built-in function getcwd>
->>> os.getcwd("/Users/jenny/IBI_2022-23/Practical7")
-Traceback (most recent call last):
-  File "<pyshell#15>", line 1, in <module>
-    os.getcwd("/Users/jenny/IBI_2022-23/Practical7")
-TypeError: posix.getcwd() takes no arguments (1 given)
-os.getcwd("/Users/jenny/IBI_2022-23/Practical7")
-Traceback (most recent call last):
-  File "<pyshell#16>", line 1, in <module>
-    os.getcwd("/Users/jenny/IBI_2022-23/Practical7")
-TypeError: posix.getcwd() takes no arguments (1 given)
-
 os.getcwd()
 '/Users/jenny/IBI_2022-23/Practical7'
 
@@ -71,12 +34,7 @@ Data columns (total 6 columns):
  5   total_deaths  7996 non-null   int64 
 dtypes: int64(4), object(2)
 memory usage: 374.9+ KB
-describe()
-         
-Traceback (most recent call last):
-  File "<pyshell#24>", line 1, in <module>
-    describe()
-NameError: name 'describe' is not defined
+  
 covid_data.describe()
          
           new_cases   new_deaths    total_cases  total_deaths
@@ -112,35 +70,8 @@ covid_data.iloc[0:10:2,0:5]
 4  2020-01-04  Afghanistan          0           0            0
 6  2020-01-06  Afghanistan          0           0            0
 8  2020-01-08  Afghanistan          0           0            0
-covid_data.iloc[0:1000:100,0:11]
-         
-           date     location  new_cases  new_deaths  total_cases  total_deaths
-0    2019-12-31  Afghanistan          0           0            0             0
-100  2020-03-27      Albania         28           1          174             6
-200  2020-03-22      Andorra         13           0           88             0
-300  2020-02-07      Armenia          0           0            0             0
-400  2020-02-15    Australia          0           0           15             0
-500  2020-02-23      Austria          0           0            0             0
-600  2020-03-02   Azerbaijan          2           0            3             0
-700  2020-03-04      Bahrain          2           0           49             0
-800  2020-02-10      Belarus          0           0            0             0
-900  2020-02-26      Belgium          0           0            1             0
-covid_data.iloc[0:1000:2,0:11]
-         
-           date     location  new_cases  new_deaths  total_cases  total_deaths
-0    2019-12-31  Afghanistan          0           0            0             0
-2    2020-01-02  Afghanistan          0           0            0             0
-4    2020-01-04  Afghanistan          0           0            0             0
-6    2020-01-06  Afghanistan          0           0            0             0
-8    2020-01-08  Afghanistan          0           0            0             0
-..          ...          ...        ...         ...          ...           ...
-990  2020-03-14      Bolivia          7           0           10             0
-992  2020-03-17      Bolivia          1           0           11             0
-994  2020-03-19      Bolivia          0           0           12             0
-996  2020-03-21      Bolivia          4           0           19             0
-998  2020-03-23      Bolivia          7           0           27             0
 
-[500 rows x 6 columns]
+
 covid_data.iloc[0:1000:100,0:2]
          
            date     location
@@ -178,12 +109,7 @@ covid_data.iloc[0:3,[0,1,3]]
 0  2019-12-31  Afghanistan           0
 1  2020-01-01  Afghanistan           0
 2  2020-01-02  Afghanistan           0
-my_columns= [Ture,True,False,True, False,False]
-         
-Traceback (most recent call last):
-  File "<pyshell#36>", line 1, in <module>
-    my_columns= [Ture,True,False,True, False,False]
-NameError: name 'Ture' is not defined
+
 my_columns=[True,True,False,True,False,False]
          
 covid_data.iloc[0:3,my_columns]
@@ -212,9 +138,7 @@ covid_data.loc[0:81,"total_cases"]
 80    114
 81    141
 Name: total_cases, Length: 82, dtype: int64
-covid_data.loc[every row where location is "Afghanistan","total_case"]
-         
-SyntaxError: invalid syntax. Perhaps you forgot a comma?
+
 covid_data.iloc[0:1000:100,1]
          
 0      Afghanistan
@@ -228,54 +152,7 @@ covid_data.iloc[0:1000:100,1]
 800        Belarus
 900        Belgium
 Name: location, dtype: object
-covid_data.loc[7996,"new_cases"]
-         
-Traceback (most recent call last):
-  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pandas/core/indexes/range.py", line 345, in get_loc
-    return self._range.index(new_key)
-ValueError: 7996 is not in range
 
-The above exception was the direct cause of the following exception:
-
-Traceback (most recent call last):
-  File "<pyshell#43>", line 1, in <module>
-    covid_data.loc[7996,"new_cases"]
-  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pandas/core/indexing.py", line 1096, in __getitem__
-    return self.obj._get_value(*key, takeable=self._takeable)
-  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pandas/core/frame.py", line 3886, in _get_value
-    row = self.index.get_loc(index)
-  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pandas/core/indexes/range.py", line 347, in get_loc
-    raise KeyError(key) from err
-KeyError: 7996
-covid_data.loc[345,"new_cases"]
-2
-covid_data.loc[7996,"new_cases"]
-Traceback (most recent call last):
-  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pandas/core/indexes/range.py", line 345, in get_loc
-    return self._range.index(new_key)
-ValueError: 7996 is not in range
-
-The above exception was the direct cause of the following exception:
-
-Traceback (most recent call last):
-  File "<pyshell#45>", line 1, in <module>
-    covid_data.loc[7996,"new_cases"]
-  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pandas/core/indexing.py", line 1096, in __getitem__
-    return self.obj._get_value(*key, takeable=self._takeable)
-  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pandas/core/frame.py", line 3886, in _get_value
-    row = self.index.get_loc(index)
-  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pandas/core/indexes/range.py", line 347, in get_loc
-    raise KeyError(key) from err
-KeyError: 7996
-covid_data.iloc[7996,2]
-Traceback (most recent call last):
-  File "<pyshell#46>", line 1, in <module>
-    covid_data.iloc[7996,2]
-  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pandas/core/indexing.py", line 1096, in __getitem__
-    return self.obj._get_value(*key, takeable=self._takeable)
-  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pandas/core/frame.py", line 3877, in _get_value
-    return series._values[index]
-IndexError: index 7996 is out of bounds for axis 0 with size 7996
 covid_data.iloc[-1,2]
 0
 covid_data.loc[-1,"new_cases"]
@@ -317,55 +194,23 @@ covid_data.loc[:,"location"]
 7994       Zimbabwe
 7995       Zimbabwe
 Name: location, Length: 7996, dtype: object
-bool("Afghanistan")
-True
-Afghanistan=True
-if not Afghanistan=False
-SyntaxError: invalid syntax
-not Afghanistan
-False
-covid_data.loc[True,:]
-Traceback (most recent call last):
-  File "<pyshell#58>", line 1, in <module>
-    covid_data.loc[True,:]
-  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pandas/core/indexing.py", line 1097, in __getitem__
-    return self._getitem_tuple(key)
-  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pandas/core/indexing.py", line 1280, in _getitem_tuple
-    return self._getitem_lowerdim(tup)
-  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pandas/core/indexing.py", line 1000, in _getitem_lowerdim
-    section = self._getitem_axis(key, axis=i)
-  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pandas/core/indexing.py", line 1342, in _getitem_axis
-    self._validate_key(key, axis)
-  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pandas/core/indexing.py", line 1151, in _validate_key
-    raise KeyError(
-KeyError: 'True: boolean label can not be used without a boolean index'
-0:81=True
-SyntaxError: illegal target for annotation
-Afghanistan=True
-covid_data.loc["Afghanistan",:]
-Traceback (most recent call last):
-  File "<pyshell#61>", line 1, in <module>
-    covid_data.loc["Afghanistan",:]
-  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pandas/core/indexing.py", line 1097, in __getitem__
-    return self._getitem_tuple(key)
-  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pandas/core/indexing.py", line 1280, in _getitem_tuple
-    return self._getitem_lowerdim(tup)
-  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pandas/core/indexing.py", line 1000, in _getitem_lowerdim
-    section = self._getitem_axis(key, axis=i)
-  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pandas/core/indexing.py", line 1343, in _getitem_axis
-    return self._get_label(key, axis=axis)
-  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pandas/core/indexing.py", line 1293, in _get_label
-    return self.obj.xs(label, axis=axis)
-  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pandas/core/generic.py", line 4095, in xs
-    loc = index.get_loc(key)
-  File "/Library/Frameworks/Python.framework/Versions/3.11/lib/python3.11/site-packages/pandas/core/indexes/range.py", line 349, in get_loc
-    raise KeyError(key)
-KeyError: 'Afghanistan'
-plt.plot(world_dates,world_new_cases,'b+')
-Traceback (most recent call last):
-  File "<pyshell#62>", line 1, in <module>
-    plt.plot(world_dates,world_new_cases,'b+')
-NameError: name 'world_dates' is not defined
-import numpy as np
-import matplotlib.pyplot as plt
+
+row = covid_data.loc[covid_data['location']=='Afghanistan']
+                
+print(row)
+                
+          date     location  new_cases  new_deaths  total_cases  total_deaths
+0   2019-12-31  Afghanistan          0           0            0             0
+1   2020-01-01  Afghanistan          0           0            0             0
+2   2020-01-02  Afghanistan          0           0            0             0
+3   2020-01-03  Afghanistan          0           0            0             0
+4   2020-01-04  Afghanistan          0           0            0             0
+..         ...          ...        ...         ...          ...           ...
+77  2020-03-27  Afghanistan          0           0           75             1
+78  2020-03-28  Afghanistan         16           1           91             2
+79  2020-03-29  Afghanistan         15           1          106             3
+80  2020-03-30  Afghanistan          8           1          114             4
+81  2020-03-31  Afghanistan         27           0          141             4
+
+[82 rows x 6 columns]
 

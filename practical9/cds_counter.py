0ff6925cd19ced543240d3254091f0a625a22859
JennyLi66 <127337307+JennyLi66@users.noreply.github.com>
2023-04-24 13:54:21 +0800

    Add files via upload

--- practical9/cds_counter.py
+++ practical9/cds_counter.py
@@ -1,1 +1,15 @@
+seq = 'ATGCAATCGACTACGATCTGAGGGCCTAA'
+start_index = seq.find('ATG')
+coding_seq_count = 0
+while start_index < len(seq):
+    stop_index = seq.find('TAA', start_index + 3)
+    stop_index = min(stop_index, seq.find('TAG', start_index + 3))
+    stop_index = min(stop_index, seq.find('TGA', start_index + 3))
+    if stop_index != -1:
+        coding_seq_count += 1
+        start_index = seq.find('ATG', stop_index + 3)
 
+    else:
+        break
+
+print('Total count of possible coding sequences:', coding_seq_count)

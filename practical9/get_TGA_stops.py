01669cd8a9aab1601f2f18b3f39f0533a76e2f05
JennyLi66 <127337307+JennyLi66@users.noreply.github.com>
2023-04-25 16:25:11 +0800

    Update get_TGA_stops.py

--- practical9/get_TGA_stops.py
+++ practical9/get_TGA_stops.py
@@ -1,15 +1,17 @@
 Python 3.11.2 (v3.11.2:878ead1ac1, Feb  7 2023, 10:02:41) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
 Type "help", "copyright", "credits" or "license()" for more information.
-with open('/Users/jenny/Desktop/IBI 1/week9/1.fa') as f:
-    nmae, seq = ", "
-    for line in f:
+with open('/Users/jenny/Desktop/IBI 1/week9/1.fa','r')as input_file, open('/Users/jenny/Desktop/IBI 1/week9/TGA_genes.fa', 'w') as output_file:
+    current_gene = ''
+    current_sequence = ''
+    for line in input_file:
         if line.startswith('>'):
-            if seq.endswith('TGA'):
-                with open('/Users/jenny/Desktop/IBI 1/week9/TGA_genes.fa', 'a') as output:
-                    output.write(f'{name}\n{seq}\n')
-                name, seq = line.strip(), ''
-            else:
-                seq += line.strip()
-        if seq.endswith('TGA'):
-            with open('/Users/jenny/Desktop/IBI 1/week9/TGA_genes.fa', 'a') as output:
-                output.write(f'{name}\n{seq}\n')
+            if current_sequence.endswith('TGA'):
+                output_file.write(current_gene + '\n')
+                output_file.write(current_sequence + '\n')
+            current_gene = line.strip()
+            current_sequence = ''
+        else:
+            current_sequence += line.strip()
+    if current_sequence.endswith('TGA'):
+        output_file.write(current_gene +'\n')
+        output_file.write(current_sequence + '\n')
